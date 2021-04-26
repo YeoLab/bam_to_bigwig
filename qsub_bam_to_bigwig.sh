8da0901b3360be8f7a8d434e4d7f4115a6a67686
Eric Kofman <ekofman@tscc-1-48.sdsc.edu>
2021-04-26 12:41:04 -0700

    path

--- qsub_bam_to_bigwig.sh
+++ qsub_bam_to_bigwig.sh
@@ -14,6 +14,6 @@ cd $PBS_O_WORKDIR
 # Argument 1 is text file where each line is the full path to an input bam
 # Argument 2 is the output directory
 
-bash bam_to_bw.sh /projects/ps-yeolab3/ekofman/RPS2_visualization/bigwigs/bam_to_bw_inputs.txt \
+bash /projects/ps-yeolab3/ekofman/RPS2_visualization/bigwigs/bam_to_bw.sh /projects/ps-yeolab3/ekofman/RPS2_visualization/bigwigs/bam_to_bw_inputs.txt \
 /projects/ps-yeolab3/ekofman/RPS2_visualization/bigwigs \
 /projects/ps-yeolab3/bay001/reference_data/hg19/hg19.chrom.sizes
